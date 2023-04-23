63659d5a7213d269d4d4320a2ec8b54b99744dc3
Eric Keis <ericpkeis@gmail.com>
2023-04-23 17:14:25 -0600

    Add vaultdata to prisma schema

---
+++ src/pages/vault.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import Navbar from "~/components/Navbar";
+
+const Vault: NextPage = () => {
+  return (
+    <>
+      <Navbar />
+
+      
+    </>
+  )
+}
+
+export default Vault;
