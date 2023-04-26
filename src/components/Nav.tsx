4f28678ce70703cb482b9071f4c4ce8e09a2e506
Eric Keis <ericpkeis@gmail.com>
2023-04-26 09:20:29 -0600

    Finish navbar improvements

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,9 @@ import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
 import { signIn, useSession } from "next-auth/react";
 import UserDropdown from "./navigation/UserDropdown";
 import { Session } from "next-auth";
+import Image from "next/image";
+import logo from ".//../../public/vault.png";
+import Link from "next/link";
 
 interface NavigationProps {
   activeTab: string | null
@@ -19,48 +22,51 @@ const Nav: React.FC<NavigationProps> = ({ activeTab = null, sessionData = null, 
       fluid={true}
       rounded={true}
       >
-        <Navbar.Brand href="https://flowbite.com/">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="mr-3 h-6 sm:h-9"
-            alt="Flowbite Logo"
+        <Navbar.Brand href="/">
+          <Image
+            src={logo}
+            className="mr-3 h-6 w-6 sm:h-9 sm:w-9"
+            alt="Logo"
           />
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           ZKA Vault
           </span>
         </Navbar.Brand>
         
-        {sessionStatus === "unauthenticated"
-          ? <div className="flex md:order-2">
-              <Button
-                onClick={() => void signIn()}
-              >
-                Log In
-              </Button>
-              <Navbar.Toggle />
-            </div>
-          : sessionStatus === "loading"
-          ? <div className="flex md:order-2">
-              <Avatar rounded={true}/>
-            </div>
-          : <UserDropdown sessionData={sessionData}/>
-        }
-        
-
-        <Navbar.Collapse>
-          <Navbar.Link
-            href="/"
-            active={activeTab === "Home"}
-          >
-            Home
-          </Navbar.Link>
-          <Navbar.Link 
-            href="/vaults"
-            active={activeTab === "Vault"}
-          >
-            Vault
-          </Navbar.Link>
-        </Navbar.Collapse>
+        <div className="md:flex md:items-center">
+          {sessionStatus === "unauthenticated"
+            ? <div className="flex md:order-2">
+                <Button
+                  onClick={() => void signIn()}
+                >
+                  Log In
+                </Button>
+                <Navbar.Toggle />
+              </div>
+            : sessionStatus === "loading"
+            ? <div className="flex md:order-2">
+                <Avatar rounded={true}/>
+              </div>
+            : <UserDropdown sessionData={sessionData}/>
+          }
+          
+          <Navbar.Collapse className="md:mx-7">
+            <Navbar.Link
+              href="/"
+              active={activeTab === "Home"}
+              as={Link}
+            >
+              Home
+            </Navbar.Link>
+            <Navbar.Link 
+              href="/vaults"
+              active={activeTab === "Vault"}
+              as={Link}
+            >
+              Public Vaults
+            </Navbar.Link>
+          </Navbar.Collapse>
+        </div>
       </Navbar>
     </>
   )
--- src/pages/vaults.tsx
+++ src/pages/vaults.tsx
@@ -7,6 +7,7 @@ import { useForm } from 'react-hook-form';
 
 import { api } from "~/utils/api";
 import { useSession } from "next-auth/react";
+import { Table } from "flowbite-react";
 
 
 const Vaults: NextPage = () => {
@@ -32,62 +33,91 @@ const Vaults: NextPage = () => {
         sessionStatus={sessionStatus}
       />
       
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-6">
-          <label 
-            htmlFor="vaultName" 
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >Vault Name</label>
-          <input 
-            type="vaultName" 
-            id="vaultName" 
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            placeholder="My Vault" 
-            required
-            maxLength={25}
-            {...register("vaultName")}
-          />
-        </div>
-        <div className="mb-6">
-          <label 
-            htmlFor="vaultPassword" 
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >Vault password</label>
-          <input 
-            type="password" 
-            id="vaultPassword" 
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            required
-            {...register("vaultPassword")}
-          />
-        </div>
-        <div className="mb-6">
-          <label 
-            htmlFor="message" 
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >Vault data</label>
-          <textarea 
-            id="message" 
-            rows={4} 
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            placeholder="Store your secure information here"
-            required
-            {...register("vaultData")}
-          ></textarea>
-        </div>
-        <button 
-          type="submit" 
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >Create Vault</button>
-      </form>
+      <h1 className="text-center my-5 font-semibold">Saved Vaults</h1>
+      <div className="flex justify-center">
+      <div className="mb-9 px-3 w-full md:w-1/2">
+        <Table striped={true}>
+          <Table.Head>
+            <Table.HeadCell>
+              Vault Name
+            </Table.HeadCell>
+            <Table.HeadCell>
+              <span className="sr-only">
+                Edit
+              </span>
+            </Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {vaults.data?.map(v => (
+              <Table.Row key={v.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  {v.name}
+                </Table.Cell>
+                <Table.Cell>
+                  <Link 
+                    href={`/vault/${v.id}`}
+                    className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  >
+                    View
+                  </Link>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </div>
+      </div>
 
-      <h1 className="mt-10">Saved Vaults</h1>
-
-      {vaults.data?.map(v => (
-        <div key={v.id}>
-          <Link href={`/vault/${v.id}`}>{v.name}</Link>
-        </div>
-      ))}
+      <div className="flex justify-center">
+        <form onSubmit={handleSubmit(onSubmit)} className="px-3 w-full md:w-1/2">
+          <div className="mb-3">
+            <label 
+              htmlFor="vaultName" 
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >Vault Name</label>
+            <input 
+              type="vaultName" 
+              id="vaultName" 
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+              placeholder="My Vault" 
+              required
+              maxLength={25}
+              {...register("vaultName")}
+            />
+          </div>
+          <div className="mb-3">
+            <label 
+              htmlFor="vaultPassword" 
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >Vault password</label>
+            <input 
+              type="password" 
+              id="vaultPassword" 
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+              required
+              {...register("vaultPassword")}
+            />
+          </div>
+          <div className="mb-3">
+            <label 
+              htmlFor="message" 
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >Vault data</label>
+            <textarea 
+              id="message" 
+              rows={4} 
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+              placeholder="Store your secure information here"
+              required
+              {...register("vaultData")}
+            ></textarea>
+          </div>
+          <button 
+            type="submit" 
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >Create Vault</button>
+        </form>
+      </div>
     </>
   )
 }
